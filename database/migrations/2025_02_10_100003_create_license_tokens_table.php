d92c118824894703970f7b1a6dd1e701508e61db
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2025-02-10 19:07:13 +0330

    Fixed a minor issue

--- database/migrations/2025_02_10_100003_create_license_tokens_table.php
+++ database/migrations/2025_02_10_100003_create_license_tokens_table.php
@@ -4,19 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
- class CreateLicenseTokensTable extends Migration {
+class CreateLicenseTokensTable extends Migration
+{
     public function up()
     {
         if (!Schema::hasTable('license_tokens')) {
-        Schema::create('license_tokens', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->integer('license_id');
-            $table->string('token', 100)->unique();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamp('expires_at')->nullable();
-            $table->timestamps();
-        });
-    }
+            Schema::create('license_tokens', function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->integer('license_id');
+                $table->string('token', 100)->unique();
+                $table->timestamp('last_used_at')->nullable();
+                $table->timestamp('expires_at')->nullable();
+                $table->timestamps();
+            });
+        }
     }
 
     public function down()
